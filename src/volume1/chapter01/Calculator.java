66933fdbd40dd3b67a9b9ae03caaad58bff9ebd5
songseongbin <ssbin0916@naver.com>
2024-01-21 15:17:16 +0900

    자바의 신 - 챕터01

---
+++ src/volume1/chapter01/Calculator.java
@@ -0,0 +1,34 @@
+package volume1.chapter01;
+
+public class Calculator {
+
+    public int add(int num1, int num2) {
+        int sum;
+        sum = num1 + num2;
+        return sum;
+    }
+
+    public int addAll(int num1, int num2, int num3) {
+        int sum;
+        sum = num1 + num2 + num3;
+        return sum;
+    }
+
+    public int subtract(int num1, int num2) {
+        int sum;
+        sum = num1 - num2;
+        return sum;
+    }
+
+    public int multiply(int num1, int num2) {
+        int sum;
+        sum = num1 * num2;
+        return sum;
+    }
+
+    public int divide(int num1, int num2) {
+        int sum;
+        sum = num1 / num2;
+        return sum;
+    }
+}
