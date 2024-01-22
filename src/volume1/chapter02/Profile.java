68d9c308b0f8e3dd8c882dabe03b5aaf82f15f99
songseongbin <ssbin0916@naver.com>
2024-01-22 13:28:57 +0900

    자바의 신 - 챕터02

---
+++ src/volume1/chapter02/Profile.java
@@ -0,0 +1,12 @@
+package volume1.chapter02;
+
+public class Profile {
+
+    public static void main(String[] args) {
+
+        String name = "이름";
+        int age = 0;
+        System.out.println("My name is " + name);
+        System.out.println("My age is " + age);
+    }
+}
